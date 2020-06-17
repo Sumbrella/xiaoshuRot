cc72248e112b9ae65249186ce5592e8e147ffb6e
e <493972246@qq.com>
2020-06-17 09:53:50 +0800

    修复bug

--- bot/plugins/_vip_send_group_message.py
+++ bot/plugins/_vip_send_group_message.py
@@ -1,31 +1,33 @@
 """
 """
-NOINPUT = 0
 
 from nonebot import on_command, CommandSession
-from nonebot import  premission as perm
+from nonebot import permission as perm
 
-@on_command('send_group_message' ,permission=perm.SUPERUSER, aliases=('发送群消息'))
+
+NOINPUT = 0
+
+
+@on_command('send_group_message', permission=perm.SUPERUSER, aliases=('发送群消息'))
 async def _send_group_message(session: CommandSession):
-	if session.state['group_id'] == NOINPUT:
-		await session.send('指令错误!')
-		return
-	else:
-		group_id = int(session.state['group_id'])
-		message = session.state['target_message']
+    if session.state['group_id'] == NOINPUT:
+        await session.send('指令错误!')
+        return
+    else:
+        group_id = int(session.state['group_id'])
+        message = session.state['target_message']
 
-		await session.bot.send(group_id=group_id, message=message)
-		await session.send('消息已发送')
+        await session.bot.send_group_msg(group_id=group_id, message=message)
+        await session.send('消息已发送')
+
 
 @_send_group_message.args_parser
 async def _(session: CommandSession):
-	arg = session.current_arg_text
-	args = arg.split('/')
-	try:
-		group_id = args[0]
-		message = args[1]
-	except Exception as e:
-		session.state['group_id'] = NOINPUT
-		session.state['target_message'] = ""
-
-
+    arg = session.current_arg_text
+    args = arg.split('/')
+    try:
+        group_id = args[0]
+        message = args[1]
+    except Exception as e:
+        session.state['group_id'] = NOINPUT
+        session.state['target_message'] = ""
